192192596a57a82132e87a79ca2797f489e4a8c2
Marcela Mejia <mejiao24@hotmail.com>
2023-09-10 18:46:07 +1000

    refactoring BEM convention and fix styling

--- assets/js/yoga.js
+++ assets/js/yoga.js
@@ -8,7 +8,7 @@ function plusSlides(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slides");
+  let slides = document.getElementsByClassName("yoga-slide");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
